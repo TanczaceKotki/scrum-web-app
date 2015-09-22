5af022b2e7cc277a5937b6071fd77d3284c5f936
Wojciech Kasperek <wojciech.kasperek@pega.com>
2015-09-22 15:07:29 +0200

    next

--- src/main/java/servlet/Signup.java
+++ src/main/java/servlet/Signup.java
@@ -1,9 +1,17 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+package servlet;
 
-// Extend HttpServlet class
-public class signup extends HttpServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/signup")
+public class Signup extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
